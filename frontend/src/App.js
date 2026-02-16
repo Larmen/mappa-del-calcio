21297da12bc587a094dd5040574ac3c500f50560
anders <larmen@live.se>
2026-02-16 10:33:11 +0100

    Styling change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,10 @@ function App() {
   return (
     <div style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
       <TodaysGames />
-      <h1>Mappa del Calcio</h1>
-      <p>Welcome to the Italian Stadium Map MVP!</p>
+      <div id="header">
+        <h1>Mappa del Calcio</h1>
+        <p>Welcome to the Italian Stadium Map MVP!</p>
+      </div>
       <div style={{flex: 1, minHeight: 0}}>
         <Map />
       </div>
